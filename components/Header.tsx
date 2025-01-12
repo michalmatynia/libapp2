90db9d09871a4554dfba36efa0e753640b046a82
Michal <mmatynia@gmail.com>
2025-01-12 18:49:36 +0100

    minuta 35

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,19 +22,23 @@ Bookwise
 </ul>
 */
 const Header = () => {
+  const pathname = usePathname();
   return (
     <header className="my-10 flex justify-between gap-5">
-      <Link href="/">
-        Bookwise
-        {/* <Image src="/icons/logo.svg" alt="logo" width={40} height={40} /> */}
-      </Link>
+      <Link href="/">Bookwise</Link>
 
       <ul className="flex flex-row items-center gap-8">
-        {/* <li>
-          <Link href="/library" className={cn( inputs: "text-base cursor-pointer capitalize"), pathname === '/library' ? 'text-light-200' : 'text-light-100'}>
+        <li>
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
             Library
           </Link>
-        </li> */}
+        </li>
       </ul>
     </header>
   );

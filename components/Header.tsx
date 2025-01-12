1d3b5033aec49056db48437f34df04d1e0088a52
Michal <mmatynia@gmail.com>
2025-01-12 02:34:18 +0100

    fd

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,20 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
+/*
+       <Link href="/">
+Bookwise
+{ <Image src="/icons/logo.svg" alt="logo" width={40} height={40} /> }
+</Link>
+
+<ul className="flex flex-row items-center gap-8">
+{ <li>
+  <Link href="/library" className={cn( inputs: "text-base cursor-pointer capitalize"), pathname === '/library' ? 'text-light-200' : 'text-light-100'}>
+    Library
+  </Link>
+</li> }
+</ul>
+*/
 const Header = () => {
   return (
     <header className="my-10 flex justify-between gap-5">

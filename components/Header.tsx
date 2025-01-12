ceccade66834272079e2e92756f09f7d8563e52e
Michal <mmatynia@gmail.com>
2025-01-12 02:02:37 +0100

    sdf

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,18 @@ import { cn } from "@/lib/utils";
 const Header = () => {
   return (
     <header className="my-10 flex justify-between gap-5">
-ad
+      <Link href="/">
+        Bookwise
+        {/* <Image src="/icons/logo.svg" alt="logo" width={40} height={40} /> */}
+      </Link>
+
+      <ul className="flex flex-row items-center gap-8">
+        {/* <li>
+          <Link href="/library" className={cn( inputs: "text-base cursor-pointer capitalize"), pathname === '/library' ? 'text-light-200' : 'text-light-100'}>
+            Library
+          </Link>
+        </li> */}
+      </ul>
     </header>
   );
 };

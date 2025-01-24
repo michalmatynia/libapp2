641639bf4113ec8ec29fe3354cc4cac3168f1ed1
Michal <mmatynia@gmail.com>
2025-01-24 01:27:55 +0100

    fd

--- app/api/auth/imagekit/route.ts
+++ app/api/auth/imagekit/route.ts
@@ -1,8 +1,15 @@
 import ImageKit from "imagekit";
 import config from "@/lib/config";
+import { NextResponse } from "next/server";
 
-const imagekit = new ImageKit({
-  publicKey: config.env.imagekit.publicKey,
-  privateKey: config.env.imagekit.privateKey,
-  urlEndpoint: config.env.imagekit.urlEndpoint,
-});
+const {
+  env: {
+    imagekit: { publicKey, privateKey, urlEndpoint },
+  },
+} = config;
+
+const imagekit = new ImageKit({ publicKey, privateKey, urlEndpoint });
+
+export async function GET() {
+  return NextResponse.json(imagekit.getAuthenticationParameters());
+}
